0bfc9cfda7bdd80b254784ba176f4705338f148a
yaroslav_els <yarter003@gmail.com>
2022-02-10 15:23:58 +0200

    interactive mode added

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
 'use strict';
 
+const readline = require('readline');
+const util = require('util');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const question = util.promisify(rl.question).bind(rl);
+
+async function ask(num) {
+    let res = await question(`${num}: `);
+    if (isNaN(res)) {
+        console.log(`Error. Expected a valid real number, got ${res} instead`);
+        return await ask(num);
+    }
+    return res;
+}
+
 function solve(arr) {
     const [a, b, c] = arr;
     console.log(`Equation is: ${a} * x^2 + ${b} * x + ${c} = 0`);
@@ -15,3 +34,27 @@ function solve(arr) {
     const res2 = ((-b - Math.sqrt(D)) / (2 * a));
     console.log(`There are 2 roots:\nx1 = ${res1}\nx2 = ${res2}`);
 }
+
+async function interactive() {
+    const vars = ['a', 'b', 'c'];
+    const nums = [];
+    for (let i = 0; i < vars.length; i++) {
+        nums.push(await ask(vars[i]));
+    }
+    solve(nums);
+}
+
+async function start() {
+    const res = await question('Start in Interactive or Non-Interactive mode? [1/2]: ');
+    switch (res) {
+        case '1':
+            await interactive();
+            process.exit();
+        case '2':
+            process.exit();
+        default:
+            await start();
+    }
+}
+
+start();
